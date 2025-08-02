4ec4ad9be5edb92798f9f8385e73b74830eff1a7
qossay.rida <qossay.rida@moizshirazigmail.onmicrosoft.com>
2025-08-02 13:50:19 +0300

    add project

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/.github/',
+  base: '/',
   plugins: [react()],
   preview: {
     port: parseInt(process.env.PORT || '4173'),
